30246a6d3c0f1b2379f77c4a3c1ab3e5b7e5f2c3
Manjeet Singh <minhas23@gmail.com>
2016-03-10 01:59:07 +0530

    max subarray change..making easy read

--- src/com/manjeet/sample/array/MaxSumArray.java
+++ src/com/manjeet/sample/array/MaxSumArray.java
@@ -3,7 +3,7 @@ package com.manjeet.sample.array;
 public class MaxSumArray {
 	
 	public static void main(String[] args) {
-		int[] array = {-1,-2,3,-45,-6,-7,-1,-9};
+		int[] array = {-1,-2,3,5,-6,1,-1,-9};
 		getMaxSumArray(array);
 	}
 	
@@ -15,17 +15,17 @@ public class MaxSumArray {
 	 */
 	public static int getMaxSumArray(int[] array){
 		
-		int max = array[0];
-		int sum = array[0];
-		
-		int start = 0;
-		int end = -1;
-		for (int i = 1; i < array.length; i++){
-			sum = Math.max(array[i], sum + array[i]);
-			max = Math.max(max, sum);
-			
-		}
-		System.out.println("maxSum =" + max + ", start=" + start + " , end="+ end);
+		int max = 0;
+		int sum = 0;
+
+		for (int i = 0; i < array.length; i++){
+	        sum = sum + array[i];
+	        if (sum < 0)
+	            sum = 0;
+	        if (max < sum)
+	            max = sum;
+	    }
+		System.out.println(max);
 		return max;
 	}
 
